4e06d28497a6c1b44b4915f42b09ad2460d54703
Phillip Bosek <phillip@chandelier647.com>
2026-01-03 05:34:58 -0500

    Initial Commit

---
+++ macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Phillip Bosek on 2026-01-03.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
